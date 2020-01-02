b92b02f71034555e54937c32e19421de6209f572
JoshuaChang <joshuachang0823@gmail.com>
2020-01-02 17:17:45 +0800

    FastJson

---
+++ arch07_fastjson/README.md
@@ -0,0 +1,7 @@
+# FastJs![FastJson](FastJson.PNG)
+
+![FastJson01](FastJson01.PNG)
+
+![FastJson02](FastJson02.PNG)
+
+![FastJson03](FastJson03.PNG)
---
+++ arch07_fastjsonlib/src/main/java/com/example/arch07_fastjsonlib/JSONException.java
@@ -0,0 +1,8 @@
+package com.example.arch07_fastjsonlib;
+
+public class JSONException extends RuntimeException {
+
+    public JSONException(String message) {
+        super(message);
+    }
+}
